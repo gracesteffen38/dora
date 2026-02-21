1d28cbdaf438c81a5ff80a922774dc7c74d33d2b
Grace Steffen <gracesteffen38@gmail.com>
2026-02-20 20:34:26 -0500

    ui fixes, adding bug report form

--- inst/app/app.R
+++ inst/app/app.R
@@ -507,7 +507,7 @@ $(document).ready(function() {
                                 class = "btn-sm btn-outline-info",
                                 icon = icon("table"),
                                 style = "margin-top: 5px; align:center;")
-            )
+            ),
 
         ),
         conditionalPanel(
